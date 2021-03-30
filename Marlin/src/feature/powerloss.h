dbae80f44ad64d0d3ca8fe9f3e5b95dde77dfb94
sinole1987 <44652024+sinole1987@users.noreply.github.com>
2021-03-30 10:44:59 -0700

    Update powerloss.h

--- Marlin/src/feature/powerloss.h
+++ Marlin/src/feature/powerloss.h
@@ -39,8 +39,8 @@
 #endif
 
 //#define DEBUG_POWER_LOSS_RECOVERY
-#define SAVE_EACH_CMD_MODE
-#define SAVE_INFO_INTERVAL_MS 0
+//#define SAVE_EACH_CMD_MODE
+//#define SAVE_INFO_INTERVAL_MS 0
 
 typedef struct {
   uint8_t valid_head;
